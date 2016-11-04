fcc3f8a30c5f27c4993bfb936fc649ea87ab0c8b
lanabia <lanabeatriz.mesquita@gmail.com>
2016-11-04 19:09:46 -0300

    Telas

--- GREatDC/app/src/main/java/br/ufc/great/greatdc/MainActivity.java
+++ GREatDC/app/src/main/java/br/ufc/great/greatdc/MainActivity.java
@@ -3,18 +3,36 @@ package br.ufc.great.greatdc;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txStatus;
+    TextView txTemperature;
+    TextView txHumidity;
+    TextView txNoise;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txStatus=(TextView)findViewById(R.id.tx_status);
+        txTemperature=(TextView)findViewById(R.id.tx_temperature);
+        txHumidity=(TextView)findViewById(R.id.tx_humidity);
+        txNoise=(TextView)findViewById(R.id.tx_noise);
 
-        //TextView myTextView=(TextView)findViewById(R.id.textBox);
+        changeFont();
+
+    }
+
+    public void changeFont(){
+
         Typeface typeFace=Typeface.createFromAsset(getAssets(),"norwester.ttf");
-        //myTextView.setTypeface(typeFace);
+        txStatus.setTypeface(typeFace);
+        txTemperature.setTypeface(typeFace);
+        txHumidity.setTypeface(typeFace);
+        txNoise.setTypeface(typeFace);
 
     }
 }
